da82754cabfef0bc192c7ff0d22c87f50b96c903
Zheka Diulin <zheka.diulin@gmail.com>
2018-06-29 19:45:11 +0300

    Судоку отображается

---
+++ SudokuLib/Cell.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuLib
+{ 
+    internal class Cell
+    {
+        #region
+        internal class EventArgs
+        {
+            public byte oldValue { get; private set; }
+            public byte newValue { get; private set; }
+
+            public EventArgs(byte oldValue, byte newValue)
+            {
+                this.oldValue = oldValue;
+                this.newValue = newValue;
+            }
+        }
+
+        internal delegate void CellEvent(EventArgs args);
+        #endregion
+        private byte pValue = 0;
+        internal byte Value
+        {
+            get => pValue;
+            set
+            {
+                if (pValue == value) return;
+                EventArgs args = new EventArgs(pValue, value);
+                pValue = value;
+
+            }
+        }
+        internal event CellEvent ValueChanged;
+    }
+}
---
+++ SudokuLib/Sudoku.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuLib
+{
+    public class Sudoku
+    {
+        Cell[,] cells = new Cell[9, 9];
+
+        public Sudoku()
+        {
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                    cells[i, j] = new Cell();
+        }
+
+        public void print()
+        {
+            for (int Y = 0; Y < 3; Y++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    for (int X = 0; X < 3; X++)
+                    {
+                        for (int x = 0; x < 3; x++)
+                            Console.Write(cells[X * 3 + x, Y * 3 + y].Value + " ");
+                        Console.Write(' ');
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
